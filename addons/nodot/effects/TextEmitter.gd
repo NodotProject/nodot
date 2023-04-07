7525789596d64b8a8335210791e0b77f33723460
Jake Cattrall <623463+krazyjakee@users.noreply.github.com>
2023-04-07 23:02:42 +0100

    simplify textemitter

--- addons/nodot/effects/TextEmitter.gd
+++ addons/nodot/effects/TextEmitter.gd
@@ -1,4 +1,4 @@
-class_name TextEmitter extends Node3D
+class_name TextEmitter extends Nodot3D
 
 @export var enabled : bool = true
 @export var font_color : Color = Color.WHITE
@@ -19,33 +19,18 @@ func _physics_process(delta: float) -> void:
 func action(text: String) -> void:
   if !enabled:
     return
+    
+  var label3d: Label3D = Label3D.new()
 
-  var sprite3d: Sprite3D = Sprite3D.new()
-  var viewport: SubViewport = SubViewport.new()
-  var label: Label = Label.new()
+  label3d.billboard = BaseMaterial3D.BILLBOARD_ENABLED
+  label3d.fixed_size = true
+  label3d.no_depth_test = true
+  label3d.text = text
+  label3d.font_size = font_size
+  label3d.modulate = font_color
 
-  viewport.add_child(label)
-  sprite3d.add_child(viewport)
-
-  sprite3d.texture = viewport.get_texture()
-  sprite3d.billboard = BaseMaterial3D.BILLBOARD_ENABLED
-  sprite3d.fixed_size = true
-  sprite3d.no_depth_test = true
-  sprite3d.pixel_size = 0.0033
-
-  viewport.disable_3d = true
-  viewport.transparent_bg = true
-  viewport.handle_input_locally = false
-
-  label.text = text
-  label.anchors_preset = Control.PRESET_FULL_RECT
-  label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
-  label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
-  label.add_theme_font_size_override("font_size", font_size)
-  label.add_theme_color_override("font_color", font_color)
-
-  add_child(sprite3d)
+  add_child(label3d)
 
   if despawn_time > 0.0:
     await get_tree().create_timer(despawn_time).timeout
-    sprite3d.queue_free()
+    label3d.queue_free()
