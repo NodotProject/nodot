4f4c951f2336997a2e5803fb8faab34eac1aa3f6
Jake Cattrall <krazyjakee@gmail.com>
2023-04-10 12:46:55 +0100

    door model

--- addons/nodot/physics/Mover3D.gd
+++ addons/nodot/physics/Mover3D.gd
@@ -23,15 +23,16 @@ var original_rotation = rotation
 var activated = false
 var target_reached = true
 
-func _process_physics(delta: float):
+func _physics_process(delta: float):
   if target_node:
     var speed = movement_speed * delta
+    var destination_rotation_radians = Vector3(deg_to_rad(destination_rotation.x), deg_to_rad(destination_rotation.y), deg_to_rad(destination_rotation.z))
     if activated:
       if destination_position:
         target_node.position = lerp(target_node.position, destination_position, speed)
       if destination_rotation:
-        target_node.rotation = lerp(target_node.rotation, destination_rotation, speed)
-      if !target_reached and target_node.position == destination_position and target_node.rotation == destination_rotation:
+        target_node.rotation = lerp(target_node.rotation, destination_rotation_radians, speed)
+      if !target_reached and target_node.position == destination_position and target_node.rotation == destination_rotation_radians:
         emit_signal("destination_reached")
         target_reached = true
     elif !one_shot:
