d6e506b77e962c294caac0af535fecbf24c8f3ae
Jake Cattrall <krazyjakee@gmail.com>
2023-04-13 10:45:36 +0100

    simpler float physics

--- addons/nodot/physics/WaterArea3D.gd
+++ addons/nodot/physics/WaterArea3D.gd
@@ -1,6 +1,7 @@
 ## An area with a water surface where the player can swim and objects can float
 class_name WaterArea3D extends Area3D
 
+@export var water_shader: ShaderMaterial = load("res://addons/nodot/shaders/clean_water.tres")
 @export var float_force: float = 1.0
 @export var force_probes: int = 9
 @export var water_drag: float = 0.05
@@ -9,7 +10,7 @@ class_name WaterArea3D extends Area3D
 @onready var default_gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 var rng = RandomNumberGenerator.new()
-var water_material = load("res://addons/nodot/shaders/clean_water.tres")
+var waterMeshInstance: MeshInstance3D = MeshInstance3D.new()
 
 # Used to track bodies
 var body_tracker: Array[RigidBody3D] = []
@@ -34,16 +35,15 @@ func _ready():
     if child is CollisionShape3D:
       collider_shape = child.shape
       
-  var waterMeshInstance = MeshInstance3D.new()
   var waterMesh = QuadMesh.new()
   waterMesh.orientation = PlaneMesh.FACE_Y
   waterMesh.size = Vector2(collider_shape.size.x, collider_shape.size.z)
   waterMesh.subdivide_width = 200
   waterMesh.subdivide_depth = 200
-  waterMesh.material = water_material
+  waterMesh.material = water_shader
   waterMeshInstance.mesh = waterMesh
   waterMeshInstance.transparency = 0.05
-  waterMeshInstance.cast_shadow = false
+  waterMeshInstance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
   waterMeshInstance.position.y = collider_shape.size.y / 2
   
   add_child(waterMeshInstance)
@@ -56,7 +56,8 @@ func _ready():
   
 func _on_body_entered(body: Node3D):
   if body is RigidBody3D and !body_tracker.has(body):
-    create_probes(body)
+    # TODO: Create better probe logic
+    # create_probes(body)
     body_tracker.append(body)
   
 func _on_body_exited(body: Node3D):
@@ -70,14 +71,26 @@ func _physics_process(delta: float):
   material.set_shader_parameter("wave_time", time)
   
   for idx in body_tracker.size():
-    var body = body_tracker[idx]
-    var probes = probe_tracker[idx]
-    for p in probes:
-      var depth = get_wave_height(p) - p.y 
+    if idx <= body_tracker.size() - 1:
+      var body = body_tracker[idx]
+      
+      # TODO: Create better probe logic
+      # var probes = probe_tracker[idx]
+      # for p in probes:
+      #   var depth = get_wave_height(body.global_position + p) - body.global_position.y
+      #  if depth > 0:
+      #    body.linear_velocity *= 1 - water_drag
+      #    body.angular_velocity *= 1 - water_angular_drag 
+      #    body.apply_force(Vector3.UP * float_force * gravity * depth, p)
+      
+      var depth = get_wave_height(body.global_position) - body.global_position.y
       if depth > 0:
         body.linear_velocity *= 1 - water_drag
-        body.angular_velocity *= 1 - water_angular_drag 
-        body.apply_force(Vector3.UP * float_force * gravity * depth, p)
+        body.angular_velocity *= 1 - water_angular_drag
+        if is_instance_valid(body):
+          # This await allows other physics to apply first
+          await get_tree().physics_frame
+          body.apply_force(Vector3.UP * float_force * gravity * depth)
     
 ## Create probe points (global position coordinates) at varying positions at the bottom of the body collider      
 func create_probes(body: RigidBody3D):
@@ -100,4 +113,4 @@ func get_wave_height(world_position: Vector3) -> float:
   var uv_y = wrapf(world_position.z / noise_scale + time * wave_speed, 0, 1)
 
   var pixel_pos = Vector2(uv_x * noise.get_width(), uv_y * noise.get_height())
-  return global_position.y + noise.get_pixelv(pixel_pos).r * height_scale;
+  return waterMeshInstance.global_position.y + noise.get_pixelv(pixel_pos).r * height_scale;
