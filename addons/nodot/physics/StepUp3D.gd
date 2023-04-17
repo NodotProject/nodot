fb43c530ab1c19faa0fad3bfc204ca8db0d89c67
Jake Cattrall <krazyjakee@gmail.com>
2023-04-17 15:40:07 +0100

    basic stepup logic

---
+++ addons/nodot/physics/StepUp3D.gd
@@ -0,0 +1,11 @@
+## A raycast powered tool to detect when the player is trying to move up some steps and will elevate the player
+class_name StepUp3D extends Nodot3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+  pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+  pass
