9e4fff415505da13c851c9d4d85f8378167cf262
Jake Cattrall <krazyjakee@gmail.com>
2023-04-18 18:44:40 +0100

    handle when fps_viewport doesn't exist

--- addons/nodot/characters/FirstPerson/FirstPersonKeyboardInput.gd
+++ addons/nodot/characters/FirstPerson/FirstPersonKeyboardInput.gd
@@ -57,7 +57,7 @@ func _ready() -> void:
       fps_viewport = child
 
 func _input(event: InputEvent) -> void:
-  if event.is_action_pressed("reload"):
+  if fps_viewport and event.is_action_pressed("reload"):
     fps_viewport.reload()
 
 func _physics_process(delta: float) -> void:
