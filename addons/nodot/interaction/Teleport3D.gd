f4c07e4e2c7ca5fa71ef9e72518fe9e50701a29f
Z3R0GT <117790541+Z3R0GT@users.noreply.github.com>
2023-04-21 21:06:32 -0500

    Update (add timer)
    - add timer to code (is automaty add node, or not add node)
    
    - fix syntax errors

--- addons/nodot/interaction/Teleport3D.gd
+++ addons/nodot/interaction/Teleport3D.gd
@@ -7,9 +7,11 @@ class_name Teleport3D extends Area3D
 ## Player node or other objective to teleport
 @export_node_path("CharacterBody3D") var Player
 
-@export_category("Size collision")
+@export_category("Others Variables")
 ## Size of collision area
 @export var size = Vector3(3.372, 3.821, 1)
+## Time to teleport (in seconds, and this variable is optional)
+@export var time_teleport : int = 0
 
 ## Get position of "Player" 
 @onready var position_player = get_node(Player)
@@ -18,9 +20,13 @@ class_name Teleport3D extends Area3D
 
 ## Collision Shape
 var node_collision : CollisionShape3D
+## Timer node
+var node_time : Timer
 
-## Check variable for telepor 
-var can_teleport = false
+## Check variable if "Player" is in "area"
+var can_teleport_area = false
+## Check variable, if "Player" wait a time
+var can_teleport_time = false
 
 func _enter_tree():
 	# add collision to scene
@@ -28,33 +34,55 @@ func _enter_tree():
 	node_collision.name = "CollisioArea"
 	add_child(node_collision)
 	
-	# Create a CollisionShape
-	var CollNode = $CollisioArea
 	# Create a Shape for "CollisioArea"
 	var Shape = BoxShape3D.new()
 	
-	#Add size to collision and add the shape to "CollisioArea"
+	# Add size to collision and add the shape to "CollisioArea"
 	Shape.size = size
 	node_collision.shape = Shape
 	
+	# optional process to create a node_timer
+	if time_teleport == 0:
+		pass
+	else: 
+		node_time = Timer.new()
+		node_time.name = "TimerFreeze"
+		add_child(node_time)
+		$TimerFreeze.wait_time = time_teleport
+		$TimerFreeze.connect("timeout", self.timer_out)
+
 	# Connect signal of "Area3D" to "Teleport3D"
 	self.connect("body_entered", body_entered)
 	self.connect("body_exited", body_exited)
 
-
-func _process(delta: float) -> void:
+func _process(_delta) -> void:
 	# Get position of target to teleport
 	var target_positon = Vector3(position_target.position.x, position_target.position.y, position_target.position.z)
 	
-	if can_teleport:
+	if can_teleport_area and can_teleport_time:
 		position_player.position = target_positon
 
 ## When "player" entered to "Teleport3D"
 func body_entered(body):
 	if body == position_player:
-		can_teleport = true
+		can_teleport_area = true
+		
+		if time_teleport == 0:
+			can_teleport_time = true
+		else:
+			$TimerFreeze.start()
 
 ## When "player" exited to "Teleport3D"
 func body_exited(body):
 	if body == position_player:
-		can_teleport = false
+		can_teleport_area = false
+		
+		if time_teleport == 0:
+			can_teleport_time = false
+		else:
+			can_teleport_time = false
+			$TimerFreeze.wait_time = time_teleport
+
+## Emitted when time end
+func timer_out():
+	can_teleport_time = true
