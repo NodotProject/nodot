21a5527aa76e78c8e3583329f9c7dcfb181fd7e8
Jake Cattrall <krazyjakee@gmail.com>
2023-04-26 18:04:12 +0100

    Save Manager (#56)
    * simple saver node and savemanager
    
    * working save logic
    
    * allow saving of custom values
    
    * last bit of docs

--- addons/nodot/plugin.gd
+++ addons/nodot/plugin.gd
@@ -7,3 +7,4 @@ func _init():
   var is_editor: bool = Engine.is_editor_hint()
   if is_editor:
     add_autoload_singleton("WindowManager", "res://addons/nodot/autoload/WindowManager.gd")
+    add_autoload_singleton("SaveManager", "res://addons/nodot/autoload/SaveManager.gd")
