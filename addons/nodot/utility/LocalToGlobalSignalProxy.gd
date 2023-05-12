20653902ced257897e94c92d51121dba28244379
Jake Cattrall <krazyjakee@gmail.com>
2023-05-12 21:02:42 +0100

    Merge GlobalSignalConnector into SignalConnector

--- addons/nodot/utility/LocalToGlobalSignalProxy.gd
+++ addons/nodot/utility/LocalToGlobalSignalProxy.gd
@@ -1,26 +1,65 @@
+@tool
 ## Proxies local signals to global signals
 class_name LocalToGlobalSignalProxy extends Nodot
 
-## The node that will emit the signal
-@export var trigger_node: Node
-## The name of the signal
-@export var trigger_signal: String = ""
+var is_editor: bool = Engine.is_editor_hint()
+
+@export_subgroup("Trigger")
+## The path of the node that will emit the signal
+@export var trigger_node: NodePath: # TODO: WARNING DO NOT CHANGE TYPE: https://github.com/godotengine/godot/issues/77012
+	set(new_node):
+		trigger_node = new_node
+		if is_editor:
+			actual_trigger_node = get_node(trigger_node)
+		notify_property_list_changed()
+@export_subgroup("Target")
 ## The global signal name
 @export var global_signal: String = ""
 ## Arguments to unbind from signal
 @export var unbind_count: int = 0
 
+## The name of the signal
+var trigger_signal: String = ""
+## The actual node that will emit the signal
+var actual_trigger_node: Node
 
 func _ready():
+	if trigger_node:
+		actual_trigger_node = get_node(trigger_node)
+	
 	if (
-		is_instance_valid(trigger_node)
+		is_instance_valid(actual_trigger_node)
 		and trigger_signal != ""
 		and global_signal != ""
 	):
 		var callback = _callback
 		if unbind_count > 0:
-			callback = Callable(_callback.unbind(unbind_count))
-		trigger_node.connect(trigger_signal, callback)
+			callback = Callable(callback.unbind(unbind_count))
+		actual_trigger_node.connect(trigger_signal, callback)
 
 func _callback():
 	GlobalSignal.trigger_signal(global_signal)
+
+func _get_property_list() -> Array[Dictionary]:
+	var property_list: Array[Dictionary] = [{
+		name = "Trigger",
+		type = TYPE_NIL,
+		usage = PROPERTY_USAGE_SUBGROUP
+	}]
+	
+	if actual_trigger_node:
+		var signal_data = actual_trigger_node.get_signal_list()
+		var signals: Array = signal_data.map(func(item): return item.name).filter(
+			func(item): return item != ""
+		)
+		signals.sort()
+
+		property_list.append({
+			"name": "trigger_signal",
+			"type": TYPE_STRING,
+			"usage": PROPERTY_USAGE_DEFAULT,
+			"hint": PROPERTY_HINT_ENUM,
+			"hint_string": ",".join(signals),
+		})
+		
+	return property_list
--- addons/nodot/utility/SignalConnector.gd
+++ addons/nodot/utility/SignalConnector.gd
@@ -1,26 +1,120 @@
+@tool
 ## Connects signals to methods
 class_name SignalConnector extends Nodot
 
-## The node that will emit the signal
-@export var trigger_node: Node
-## The name of the signal
-@export var trigger_signal: String = "interacted"
-## The node that has the method
-@export var target_node: Node
-## The name of the method
-@export var target_method: String = "action"
-## Arguments to unbind from signal
-@export var unbind_count: int = 0
+var is_editor: bool = Engine.is_editor_hint()
 
+@export_subgroup("Trigger")
+## Whether to use global signals as the trigger
+@export var use_global_signal: bool = false:
+	set(new_value):
+		use_global_signal = new_value
+		if is_editor:
+			actual_trigger_node = get_node(trigger_node)
+		notify_property_list_changed()
+
+@export_subgroup("Target")
+## The node that has the method
+@export var target_node: NodePath: # WARNING DO NOT CHANGE TYPE: https://github.com/godotengine/godot/issues/77012
+	set(new_node):
+		target_node = new_node
+		if is_editor:
+			actual_target_node = get_node(target_node)
+		notify_property_list_changed()
+
+@export_subgroup("Argument Binding")
+## Arguments to unbind from signal
+@export var method_unbind_count: int = 0
+
+## The path of the node that will trigger the signal
+var trigger_node: NodePath: # WARNING DO NOT CHANGE TYPE: https://github.com/godotengine/godot/issues/77012
+	set(new_node):
+		trigger_node = new_node
+		if is_editor:
+			actual_trigger_node = get_node(trigger_node)
+		notify_property_list_changed()
+## The node that will emit the signal
+var actual_trigger_node: Node
+
+## The name of the signal
+var trigger_signal: String = ""
+## The path of the node that will execute the method
+var actual_target_node: Node
+## The name of the target node method to execute
+var target_method: String = "action"
 
 func _ready():
-	if (
-		is_instance_valid(trigger_node)
-		and is_instance_valid(target_node)
-		and trigger_signal != ""
-		and target_method != ""
-	):
-		var callback = target_node[target_method]
-		if unbind_count > 0:
-			callback = Callable(target_node[target_method].unbind(unbind_count))
-		trigger_node.connect(trigger_signal, callback)
+	if trigger_node:
+		actual_trigger_node = get_node(trigger_node)
+	if target_node:
+		actual_target_node = get_node(target_node)
+	
+	if target_method == "" or !is_instance_valid(actual_target_node): return
+	
+	if use_global_signal:
+		if trigger_signal != "":
+			GlobalSignal.add_listener(trigger_signal, actual_target_node, target_method)
+	else:
+		var callback = actual_target_node[target_method]
+		if method_unbind_count > 0:
+			callback = Callable(actual_target_node[target_method].unbind(method_unbind_count))
+		actual_trigger_node.connect(trigger_signal, callback)
+
+func _get_property_list() -> Array[Dictionary]:
+	var property_list: Array[Dictionary] = [{
+		name = "Trigger",
+		type = TYPE_NIL,
+		usage = PROPERTY_USAGE_SUBGROUP
+	}]
+	
+	if use_global_signal:
+		property_list.append({
+			"name": "trigger_signal",
+			"type": TYPE_STRING,
+			"usage": PROPERTY_USAGE_DEFAULT,
+			"hint": PROPERTY_HINT_NONE,
+			"hint_string": ""
+		})
+	else:
+		property_list.append({
+			"name": "trigger_node",
+			"type": TYPE_NODE_PATH,
+			"usage": PROPERTY_USAGE_DEFAULT,
+			"hint": PROPERTY_HINT_NODE_PATH_VALID_TYPES
+		})
+		
+		if actual_trigger_node:
+			var signal_data = actual_trigger_node.get_signal_list()
+			var signals: Array = signal_data.map(func(item): return item.name).filter(
+				func(item): return item != ""
+			)
+			signals.sort()
+
+			property_list.append({
+				"name": "trigger_signal",
+				"type": TYPE_STRING,
+				"usage": PROPERTY_USAGE_DEFAULT,
+				"hint": PROPERTY_HINT_ENUM,
+				"hint_string": ",".join(signals),
+			})
+			
+	if actual_target_node:
+		var method_data = actual_target_node.get_method_list()
+		var methods = method_data.map(func(item): return item.name).filter(
+			func(item): return item != ""
+		)
+		methods.sort()
+			
+		property_list.append_array([{
+			name = "Target",
+			type = TYPE_NIL,
+			usage = PROPERTY_USAGE_SUBGROUP
+		}, {
+			"name": "target_method",
+			"type": TYPE_STRING,
+			"usage": PROPERTY_USAGE_DEFAULT,
+			"hint": PROPERTY_HINT_ENUM,
+			"hint_string": ",".join(methods),
+		}])
+		
+	return property_list
