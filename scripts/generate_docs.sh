7b832dafed29ed1e4e2bcc7fe24c731d6cb4dc70
Jake Cattrall <krazyjakee@gmail.com>
2023-05-21 21:33:00 +0100

    update local docs generation (#97)

--- scripts/generate_docs.sh
+++ scripts/generate_docs.sh
@@ -2,9 +2,9 @@
 
 # npm i -g krazyjakee/GDScriptify2
 # npm i -g markdown-folder-to-html
-rm -rf docs/src
-rm -rf docs/addons
-gdscriptify2 -o docs/src
+rm -rf docs
+gdscriptify2 -d addons/nodot -o docs/src
+mv addons/nodot/docs .
 sed -i 's/\\/\//g' docs/src/index.md
 markdown-folder-to-html docs/src
 mv docs/_src/* docs
