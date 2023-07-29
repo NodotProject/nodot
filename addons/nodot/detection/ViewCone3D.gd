4067b243fa5f9eb648bf36b4b0c2e8dc8959f802
Jake Cattrall <krazyjakee@gmail.com>
2023-07-29 20:08:34 +0100

    Viewcone fix (#135)
    * fix for #121
    
    * small optimization
    
    * add viewcone signals

--- addons/nodot/detection/ViewCone3D.gd
+++ addons/nodot/detection/ViewCone3D.gd
@@ -1,68 +1,60 @@
 @tool
-## A cone shaped ShapeCast3D
-class_name ViewCone3D extends ShapeCast3D
+## A SpotLight3D that detects nodes that enter it's vision
+class_name ViewCone3D extends SpotLight3D
 
-## The radius of the viewcone
-@export var radius: float = 10.0: set = _radius_set
-## The distance of sight
-@export var distance: float = 100.0: set = _distance_set
-## Triggered by RigidBodies
-@export var triggered_by_rigidbodies: bool = false
-## Triggered by CharacterBodies
-@export var triggered_by_character_bodies: bool = true
+@export var enabled: bool = true
+@export var detection_group: String = "detectable"
+@export var detection_color: Color = Color.RED
+@export_flags_3d_physics var collision_layer: int = 0
 
-## A body has entered the viewcone
-signal body_entered(body: Node3D)
-## A body has exited the viewcone
-signal body_exited(body: Node3D)
+signal body_detected(body: Node3D)
+signal body_lost(body: Node3D)
 
-var colliding_bodies: Array[Node3D] = []
+var original_color = light_color
+var detected = false
+var last_detected_body: Node3D
 
-func _enter_tree() -> void:
-	_rebuild_cone()
-
-# TODO: Should use a raycast to detect the viewcone actually has line of sight
 func _physics_process(_delta):
-	if is_colliding():
-		var collision_count = get_collision_count()
-		var current_colliders := []
-		for i in collision_count:
-			var collider = get_collider(i)
-			if (triggered_by_rigidbodies and collider is RigidBody3D) or (triggered_by_character_bodies and collider is CharacterBody3D):
+	if !enabled:
+		return
+	
+	var detected_this_pass = false
+	var cam_pos = global_transform.origin
+	var max_distance_squared = pow(spot_range, 2)
+	var max_angle = deg_to_rad(spot_angle)
+	light_color = original_color
+	
+	for body in get_tree().get_nodes_in_group(detection_group):
+		var space_state = get_world_3d().direct_space_state
+		var body_pos = body.global_transform.origin
+		if cam_pos.distance_squared_to(body_pos) > max_distance_squared:
+			continue
+
+		var cam_facing = -global_transform.basis.z
+		var cam_to_body =  cam_pos.direction_to(body_pos)
+		var cam_to_body_norm = cam_to_body.normalized()
+		var cos_angle = cam_to_body_norm.dot(cam_facing)
+		var angle = acos(cos_angle)
+
+		if angle < max_angle:
+			var params = PhysicsRayQueryParameters3D.new()
 			
-				current_colliders.append(collider)
+			if collision_layer != 0:
+				params.collision_mask = collision_layer
+			params.from = cam_pos
+			params.to = body_pos
 
-				if !colliding_bodies.has(collider):
-					colliding_bodies.append(collider)
-					emit_signal("body_entered", collider)
-
-		var new_colliding_bodies = colliding_bodies
-		for i in colliding_bodies.size():
-			if i <= colliding_bodies.size() - 1:
-				var collider = colliding_bodies[i]
-				if !current_colliders.has(collider):
-					emit_signal("body_exited", collider)
-					new_colliding_bodies.remove_at(i)
-		colliding_bodies = new_colliding_bodies
-
-func _radius_set(new_value: float):
-	radius = new_value
-	_rebuild_cone()
-
-func _distance_set(new_value: float):
-	distance = new_value
-	_rebuild_cone()
-
-func _rebuild_cone():
-	shape = ConvexPolygonShape3D.new()
-	var vertices: Array[Vector3] = [Vector3.ZERO]
-
-	# Generate the vertices of the cone.
-	for i in range(0, 240):
-		var angle = i * PI / 7.5
-		var x = cos(angle) * radius
-		var y = sin(angle) * radius
-		var z = distance
-		vertices.append(Vector3(x, y, z))
-
-	shape.points = vertices
+			var result = space_state.intersect_ray(params)
+			if result and result.collider == body:
+				detected_this_pass = true
+				light_color = detection_color
+				if !detected:
+					detected = true
+					if body.has_method("detected"):
+						body.detected()
+					last_detected_body = body
+					emit_signal("body_detected", body)
+	
+	if detected_this_pass == false and detected == true:
+		detected = false
+		emit_signal("body_lost", last_detected_body)
